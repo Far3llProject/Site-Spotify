38ec6077e335223f8dd6b704475a1aac59d44e08
Rafael Martins de Castro <42079080+Far3llProject@users.noreply.github.com>
2020-05-08 17:29:33 -0300

    update

--- js/active.js
+++ js/active.js
@@ -2,8 +2,10 @@ function toggle(){
   let header = document.getElementById("header")
   let socials = document.getElementById("socials")
   let body = document.getElementById("body")
+  let menu = document.getElementById('menu')
   header.classList.toggle('header__active')
   socials.classList.toggle('footer__menu-link-socials-hide')
   body.classList.toggle('body__blocked')
+  menu.classList.toggle('menu__button--show')
 }
 
