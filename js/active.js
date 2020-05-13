aa5a632a6993a5200443478d19222b1643be55f2
Rafael Martins de Castro <42079080+Far3llProject@users.noreply.github.com>
2020-05-13 11:18:32 -0300

    update

--- js/active.js
+++ js/active.js
@@ -1,11 +1,22 @@
-function toggle(){
+function toggle() {
   let header = document.getElementById("header")
   let socials = document.getElementById("socials")
   let body = document.getElementById("body")
-  let menu = document.getElementById('menu')
   header.classList.toggle('header__active')
   socials.classList.toggle('footer__menu-link-socials-hide')
   body.classList.toggle('body__blocked')
-  menu.classList.toggle('menu__button--show')
 }
 
+function Perfil() {
+  let menu = document.getElementById('menu')
+  menu.classList.toggle('menu__button--show')
+  let header = document.getElementById("header")
+  header.classList.toggle('header__active')
+}
+
+// function mudar() {
+//   const element = document.querySelector('[data-toggle]')
+//   const value = element.dataset.toggle
+//   console.log(value)
+  
+// }
